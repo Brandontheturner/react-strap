573627b9a851b94a41a800ea7813f4d2f8765a7c
Brandon Turner <brandontheturner@gmail.com>
2019-01-17 18:34:02 -0600

    added some stuff

--- src/App.js
+++ src/App.js
@@ -1,48 +1,34 @@
-import React, { Component } from "react";
+import React, { BootStrap, Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "reactstrap";
 
 class App extends Component {
   render() {
     return (
-      <div className="jumbotron">
-        <h1 className="display-2">Brandon Turner</h1>
-        <p className="lead">Full Stack Web Developer</p>
-        <hr className="my-2" />
-        <p>
-          <ul>
-            <li className="html5">HTML5</li>
-            <li>CSS3</li>
-            <li>javaScript</li>
-            <li>Git/GitHub</li>
-            <li>React</li>
-            <li>BootStrap</li>
-          </ul>
-        </p>
-        <p className="lead">
-          <a className="btn btn-primary btn-lg" href="#!" role="button">
-            Click Me!!
-          </a>
-        </p>
-        <span class="badge badge-warning">Warning</span>
-
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React-Strap
-            </a>
-            <span className="badge badge-danger">
-              <p>Danger Will Robinson</p>
-            </span>
-          </header>
-        </div>
+      <div className="App">
+        <header className="App-header">
+          <nav className="navBar">
+            <ul className="list">
+              <li>
+                <button>Home</button>
+              </li>
+              <li>
+                <button>About</button>
+              </li>
+              <li>
+                <button>Contact</button>
+              </li>
+              <li>
+                <button>Social</button>
+              </li>
+            </ul>
+          </nav>
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="hero">
+            <h1 className="name">Brandon Turner</h1>
+            <h2 className="title">Full Stack Web Developer</h2>
+          </div>
+        </header>
       </div>
     );
   }
